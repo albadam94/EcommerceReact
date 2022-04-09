f2f2ebd28043ab6b6765c8ac6ef23e21ae050922
Brayan Albadam <blackthorn_1994@hotmail.com>
2022-04-09 14:15:37 -0500

    carrito

--- src/Components/Zapatos.jsx
+++ src/Components/Zapatos.jsx
@@ -14,7 +14,7 @@ export default function ProductSimple(props) {
 			<Box role={"group"} p={10} maxW={"350px"} w={"full"} bg={useColorModeValue("white", "gray.500")} boxShadow={"2xl"} rounded={"xl"} pos={"relative"} zIndex={1}>
 			<Box rounded={"md"} mt={-10} pos={"relative"} height={"230px"} _after={{ transition: "all .4s ease", content: '""', w: "full", h: "full", pos: "absolute", top: 5, left: 0,
 				backgroundImage: `url($"${props.imagen}")`, filter: "blur(15px)", zIndex: -1, }} _groupHover={{ _after: { filter: "blur(20px)",},}}>
-				<Image rounded={"lg"} height={230} width={282} objectFit={"cover"} src={props.imagen} />
+				<Image rounded={"lg"} height={240} width={282} objectFit={"cover"} src={props.imagen} />
 			</Box>
 			<Stack pt={10} align={"center"}>
 				<Heading
